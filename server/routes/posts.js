bad0970e61a56a570615a77aa0fa05fed5830693
nafiur63 <nafiur63@gmail.com>
2020-11-30 23:25:02 +0600

    first commit

---
+++ server/routes/posts.js
@@ -0,0 +1,10 @@
+import express from "express";
+const router = express.Router()
+import { getPosts,createPost,updatePost } from "../controllers/posts.js";
+
+router.get('/', getPosts)
+router.post('/',createPost)
+router.patch('/:id',updatePost)
+
+
+export default router
